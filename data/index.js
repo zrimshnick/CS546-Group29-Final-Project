f409a640a3082ce34ed8ad0f17f5678fc4439207
Sean Maida <smaida@stevens.edu>
2024-04-10 01:03:58 -0400

    updated with workouts collection

--- data/index.js
+++ data/index.js
@@ -1,3 +1,5 @@
 import usersDataFunctions from './users.js';
+import workoutDataFunctions from './workouts.js';
 
 export const productsData = usersDataFunctions;
+export const workoutData = workoutDataFunctions
