5cba9bea10e151444c20839a2022d04185a2f32e
Sean Maida <smaida@stevens.edu>
2024-04-10 23:31:04 -0400

    added exercise

--- data/index.js
+++ data/index.js
@@ -1,5 +1,7 @@
 import usersDataFunctions from './users.js';
 import workoutDataFunctions from './workouts.js';
+import exerciseDataFunctions from './exercise.js';
 
 export const productsData = usersDataFunctions;
-export const workoutData = workoutDataFunctions
+export const workoutData = workoutDataFunctions;
+export const exerciseData = exerciseDataFunctions;
