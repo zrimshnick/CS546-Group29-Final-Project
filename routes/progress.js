6fd59cd471cf7bd15ade97569ca87a12d1fb3945
Sean Maida <smaida@stevens.edu>
2024-04-29 14:35:41 -0400

    edited routes file

--- routes/progress.js
+++ routes/progress.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 router.route("/").get(async (req, res) => {
-    res.render("progressPage", {Title: "Tracklete | Progress Page"});
+    res.render("progressPage", {title: "Tracklete | Progress Page"});
   });
   
   export default router;
