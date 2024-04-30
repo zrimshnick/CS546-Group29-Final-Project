bdd7da48f97f7fbeada16f0ae2b8a0900383b4c9
Sean Maida <smaida@stevens.edu>
2024-04-29 22:45:18 -0400

    added posts to feed page

---
+++ public/js/feed-client.js
@@ -0,0 +1,26 @@
+console.log("feed page js is linked");
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Select all placeholder elements
+    let placeholders = document.querySelectorAll(
+      ".profile-post-comments-placeholder"
+    );
+  
+    // Add click event listener to each placeholder
+    placeholders.forEach(function (placeholder) {
+      placeholder.addEventListener("click", function () {
+        let commentsContainer = placeholder.nextElementSibling;
+        console.log(commentsContainer);
+  
+        if (commentsContainer.classList.contains("hidden")) {
+          commentsContainer.classList.remove("hidden");
+          commentsContainer.classList.add("flex");
+          placeholder.textContent = "Close comments";
+        } else {
+          commentsContainer.classList.add("hidden");
+          commentsContainer.classList.remove("flex");
+          placeholder.textContent = "View comments";
+        }
+      });
+    });
+  });
