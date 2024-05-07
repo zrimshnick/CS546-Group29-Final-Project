43d6a23ede1357784f3c8774a471a573fefb59fc
kshagalov <kshagalov@gmail.com>
2024-05-06 22:53:43 -0400

    correct version of helper file from main

--- helpers.js
+++ helpers.js
@@ -42,12 +42,17 @@ function checkValidName(param, name){
 }
 
 function checkValidUsername(param){
-  if (param.length < 5 || param.length > 10) throw 'Username must be between 5 and 10 characters long';
-  const usernameRegex = /^[^a-zA-Z0-9]+$/;
+  const usernameRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+  if (param.length < 3 || param.length > 12){
+    throw 'username is too long or too short';
+  }
 
   if (usernameRegex.test(param)){
-    throw 'username cannot only contain special characters';
+    throw 'username cannot any contain special characters'
   }
+
+  return param;
 }
 
 function checkValidPassword(password, name) {
